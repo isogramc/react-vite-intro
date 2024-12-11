4d6145b64a8f68edf5152775c39b059808a574e7
theresajvrensburg <theresa@isogramconsulting.com>
2024-12-11 18:03:04 +0100

    deploy01

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-vite-intro/',
   plugins: [react()],
-  base: 'https://isogramc.github.io/react-vite-intro/'
 })
